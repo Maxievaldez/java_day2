e2087ec35f6f3ce22bf4c9a937d0c3a743c334cf
Aishee-ops <2405410@kiit.ac.in>
2025-12-17 23:58:40 +0530

    first commit

---
+++ StudentDetails.java
@@ -0,0 +1,8 @@
+class StudentDetails {
+    public static void main(String[] args) {
+        System.out.println("Name: Aishee Maity");
+        System.out.println("Roll Number: 2405410");
+        System.out.println("Section: CSE 7");
+        System.out.println("Branch: Computer Science Engineering");
+    }
+}
